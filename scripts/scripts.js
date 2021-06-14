07e5b55eff8ac521b473149caa2ec7fb09e15ee0
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2021-06-14 17:45:16 +0100

    Added assessement class

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -131,6 +131,37 @@ function modules2(name, moduleCode, numofAssessments, average, credits){
 
 }
 
+class assessment{
+    constructor(name, yourScore, totalMarks, worth){
+        this.name = name;
+        this.score = yourScore;
+        this.totalMarks = totalMarks;
+        this.percent = this.calcPercentage(yourScore, totalMarks);
+        this.worth = `${worth}%`;
+        this.grade = this.calcGrade(this.percent);
+    }
+
+    calcPercentage(yourScore, totalMarks){
+        let percent = ((yourScore / totalMarks) * 100);
+        percent = Number(percent.toFixed(1));
+        return percent;
+    }
+
+    calcGrade(percent){
+        if(percent >= 70){
+            return "1st";
+        } else if(percent >= 60 && percent < 70){
+            return "2.1";
+        }else if(percent >= 50 && percent < 60){
+            return "2.2";
+        }else if(percent >= 40 && percent < 50){
+            return "3rd";
+        } else {
+            return "Did not pass";
+        }
+    }
+}
+
 
 class modules{
     // this.name = name;
@@ -140,28 +171,29 @@ class modules{
     // this.credits = credits;
     // this.assessments = {};
 
-    constructor(name, moduleCode, numofAssessments credits){
+    constructor(name, moduleCode, numofAssessments, credits){
         this.name = name;
         this.moduleCode = moduleCode;
         this.numOfAssessments = numofAssessments;
         this.average;
         this.credits = credits;
+        // an assessment object containing other objects;
         this.assessments = {};
     };
     
-
     //methods
 
     //returns a string representation of the fields in the object
-    toString (){
-        console.log(`Databases ${this.moduleCode} is worth ${this.credits} credits and has ${this.calcNumOfAssessments()} asssessments`);
+    details(){
+        console.log(`Databases ${this.moduleCode} is worth ${this.credits} credits and has ${this.numOfAssessments} assessments.
+You have done already done ${this.calcNumOfAssessments()} asssessment(s).`);
     };
 
     //adds a new assessment to the module
-    addAssessment(name, grade){
-        this.assessments[name] = grade;
-        this.numofAssessments++;
-    };
+
+    addAssessment(name, yourScore, totalMarks, worth){
+        this.assessments[name] = new assessment(name, yourScore, totalMarks, worth);
+    }
 
     //calulates the number of assessments in the module
     calcNumOfAssessments(){
@@ -211,19 +243,24 @@ class modules{
 
 
 
+// Tests
 
+let databases = new modules("Databases", "COMP1121", 4, 10);
 
-
-let databases = new modules("Databases", "COMP1121", 4, 0, 20);
-
-console.log(databases.name)
+console.log(databases.name);
 console.log(databases.assessments);
 
-databases.assessments["coursework_1"] = 0;
-console.log(databases.assessments);
-console.log(databases.credits)
 
-databases.toString();
+
+
+databases.addAssessment("coursework_1", 37, 40, 30);
+databases.addAssessment("coursework_2", 29, 30, 30);
+databases.addAssessment("coursework_3", 20, 30, 30);
+console.log(databases.assessments);
+console.log(databases.credits);
+console.log(databases.assessments.coursework_3.score);
+
+databases.details();
 
 // console.log(module.toString());
 // module.calcAverage();
